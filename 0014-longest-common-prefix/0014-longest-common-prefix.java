0c324ea881bd6780135b2b3b24c37b916fb3c4de
ASHISH VERMA <yash20012004@gmail.com>
2025-04-16 20:16:41 +0530

    Time: 0 ms (100%), Space: 41.6 MB (62.71%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -7,11 +7,8 @@ class Solution {
         String prefix = strs[0];
         
         for (int i = 1; i < strs.length; i++) {
-            // Check if the current prefix is a prefix of strs[i]
             while (strs[i].indexOf(prefix) != 0) {
-                // Reduce the prefix by one character
                 prefix = prefix.substring(0, prefix.length() - 1);
-                // If prefix becomes empty, return ""
                 if (prefix.isEmpty()) {
                     return "";
                 }
