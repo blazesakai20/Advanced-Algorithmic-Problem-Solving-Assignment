70872e1b04f7fdab6579daee3084ea77679dfd7e
ASHISH VERMA <yash20012004@gmail.com>
2025-04-26 22:51:30 +0530

    Time: 1 ms (93.03%), Space: 45.7 MB (44.92%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -1,25 +1,20 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        int n = nums.length;
-        int[] tempr = new int[n];
-        int[] templ = new int[n];
-
-        int leftSum = 0;
-        int rightSum = 0;
-
-        for(int i = 0; i < n ;i++){
-            leftSum += nums[i];
-            templ[i] = leftSum;
+        int prefixsum[] = new int[nums.length];
+        int suffixsum[] = new int[nums.length];
+        prefixsum[0] = nums[0];
+        suffixsum[nums.length-1] = nums[nums.length-1];
+        for(int i = 1 ; i<nums.length; i++){
+            prefixsum[i] = prefixsum[i-1] + nums[i];
         }
-        for(int i = n - 1; i >= 0 ; i--){
-            rightSum += nums[i];
-            tempr[i] = rightSum;
+          for(int j = nums.length-2 ; j>=0; j--){
+            suffixsum[j] = suffixsum[j+1] +  nums[j];
         }
-        for(int i = 0; i < n; i++){
-            if(templ[i] == tempr[i]){
+        for (int i = 0; i <= nums.length-1; i++) {
+            if (prefixsum[i] == suffixsum[i]) {
                 return i;
-            }
+            } 
         }
         return -1;
-    }
+ }
 }
